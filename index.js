3927a35d8a72d341438f76ed04fc866c8feca1da
rohithagannamaneedi <rohithagannamaneedi@gmail.com>
2025-03-21 14:05:16 +0530

    test

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require("mongoose");
+const User = require("./schema")
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 const port = 3010;
 
+app.use(express.json());
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.post('/api/users',async(req,res)=>{
+  try{
+      const {name,email,password} = req.body;
+      if(!email || !name || !password){
+        return  res.status(400).send({msg:"All field are required"});
+      }
+      const existingUser = await User.findOne({email});
+      if(existingUser){
+          return res.status(400).send({msg:"User already exisits"});
+      }
+
+      const data = new User({name,email,password});
+      await data.save();
+      return res.status(201).send({msg:"User created successfullyy.."});
+
+      
+  } catch (error) {
+      return res.status(400).send({msg:"Something went wrong"})
+  }
+
+})
+
+app.listen(port,async () => {
+  try{
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to database")
+    console.log(`Example app listening at http://localhost:${port}`);
+
+  }
+  catch(error){
+    console.log("Error connecting to database");
+  }
+  
 });
